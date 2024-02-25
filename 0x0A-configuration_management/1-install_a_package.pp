003933c1dcfc37a376d29c119deffde308450493
ayo <findtamilore@gmail.com>
2024-02-25 12:25:08 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# Puppet code to manage the installation of Flask using pip3.
+# Ensure python3.8 is installed
+package { 'python3':
+  ensure => 'installed',
+}
 
+# Puppet code to manage the installation of Flask using pip3.
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
