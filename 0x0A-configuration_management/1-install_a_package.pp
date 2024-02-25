bcba71198f876623d3ee6f083dfe043981e6214b
ayo <findtamilore@gmail.com>
2024-02-25 13:01:41 +0100

    additional check

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,4 +7,13 @@ package { 'python3':
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  before   => Exec['downgrade_werkzeug'],
+}
+
+# Downgrade Werkzeug to a specific version
+exec { 'downgrade_werkzeug':
+  command => 'pip3 install werkzeug==2.1.1',
+  path    => '/usr/bin',
+  # checks if the version is 2.1.1 before downgrading
+  unless  => 'pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
