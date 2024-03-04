ccc6f701c03bf0024c5827ae82d364e466ebe36a
tami-cp0 <findtamilore>
2024-03-04 15:53:43 +0100

    added  manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,31 +6,16 @@ exec {'update':
 } ->
 
 package {'nginx':
-  ensure => 'installed',
+  ensure => 'present',
 } ->
 
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-} ->
-
-exec {'Hello':
-  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  provider => shell,
-} -> 
-
-# sets up redirection
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/github.com\/tami-cp0\/;\\n\\t}/" /etc/nginx/sites-available/default':
-  provider => shell,
-} ->
-
-# SET custom response header
+# custom response header
 exec {'header':
   command => 'hostname=$(hostname) ; echo "add_header X-Served-By $hostname;" | sudo tee /etc/nginx/conf.d/custom_header.conf',
   provider => shell,
 } ->
 
-# Restart service
+# start service
 exec {'nginx_restart':
-  command  => 'service nginx restart',
+  command  => 'service nginx start',
 }
