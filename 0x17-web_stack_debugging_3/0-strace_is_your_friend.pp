948b660a15abdb40f63a4ed7ad46d5027f38cae0
tami-cp0 <findtamilore>
2024-04-11 20:34:27 +0100

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,10 @@
 # puppet code that fixes typo
 exec { 'Fix-wordpress':
-  command  => "/bin/grep -q \"require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );\" /var/www/html/wp-settings.php && sed -i \"s|require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );|require_once( ABSPATH . WPINC . '/class-wp-locale.php' );|g\" /var/www/html/wp-settings.php",
-  onlyif   => "/bin/grep \"require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );\" /var/www/html/wp-settings.php",
+  command => "/bin/grep -q \"require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );\" \
+              /var/www/html/wp-settings.php && \
+              sed -i \"s|require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );|\
+              require_once( ABSPATH . WPINC . '/class-wp-locale.php' );|g\" \
+              /var/www/html/wp-settings.php",
+  onlyif  => "/bin/grep \"require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );\" /var/www/html/wp-settings.php",
 }
+
