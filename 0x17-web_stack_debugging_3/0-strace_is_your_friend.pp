af0986ffa4ea9eaf77c4cf4ed07663c63b666ba3
tami-cp0 <findtamilore>
2024-04-11 12:23:28 +0100

    fixed final bug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-file { '/var/www/html/wp-settings.php':
-  ensure  => present,
-  content => replace("require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );", "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );"),
+exec { 'Fix-wordpress':
+  command  => "/bin/grep -q \"require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );\" /var/www/html/wp-settings.php && sed -i \"s|require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );|require_once( ABSPATH . WPINC . '/class-wp-locale.php' );|g\" /var/www/html/wp-settings.php",
+  onlyif   => "/bin/grep \"require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );\" /var/www/html/wp-settings.php",
 }
